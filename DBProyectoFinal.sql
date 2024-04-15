a236dc4172a436cf015e76f1b69dd9e45f49c66d
maulj2 <86072608+maulj2@users.noreply.github.com>
2024-04-15 16:31:10 -0600

    Cambios Bd

--- DBProyectoFinal.sql
+++ DBProyectoFinal.sql
@@ -5,14 +5,13 @@ USE TIENDAVIDEOJUEGOS;
 
 -- Tabla de productos
 CREATE TABLE Productos (
-    IDProducto INT PRIMARY KEY,
+    IDProducto INT IDENTITY(1,1) PRIMARY KEY,
     NombreProducto VARCHAR(255) NOT NULL,
     Precio DECIMAL(10, 2) NOT NULL,
     FechaPublicacion DATE NOT NULL,
     DisponibilidadInventario INT NOT NULL,
     VideoJuegoURL VARCHAR(255),
-    EstadoProducto VARCHAR(50) NOT NULL,
-    IDPlataforma INT,
+    EstadoProducto VARCHAR(50) NOT NULL DEFAULT 'Activo'
 );
 
 -- Tabla de usuarios
@@ -28,11 +27,11 @@ CREATE TABLE usuario (
 
 -- Tabla de direcciones de usuarios
 CREATE TABLE Direcciones (
-    IDDireccion INT PRIMARY KEY,
+    IDDireccion INT PRIMARY KEY IDENTITY(1,1),
     IDUsuario INT,
     Calle VARCHAR(255) NOT NULL,
     Ciudad VARCHAR(255) NOT NULL,
-    Estado VARCHAR(255) NOT NULL,
+    Estado VARCHAR(255) NOT NULL DEFAULT 'Activo',
     CodigoPostal VARCHAR(10) NOT NULL,
 	CONSTRAINT FK_Direcciones_Usuarios FOREIGN KEY (IDUsuario) REFERENCES usuario(IDUsuario),
 
@@ -51,16 +50,16 @@ CREATE TABLE UsuariosMetodosPago (
 
 -- Tabla de pedidos
 CREATE TABLE Pedidos (
-    IDPedido INT PRIMARY KEY,
+    IDPedido INT IDENTITY(1,1) PRIMARY KEY,
     IDUsuario INT,
-    FechaPedido DATE NOT NULL,
+    FechaPedido DATE NOT NULL DEFAULT GETDATE(),
 	CONSTRAINT FK_Pedidos_Usuarios FOREIGN KEY (IDUsuario) REFERENCES usuario(IDUsuario),
 
 );
 
 -- Tabla de detalles del pedido
 CREATE TABLE DetallesPedido (
-    IDDetalle INT PRIMARY KEY,
+    IDDetalle INT PRIMARY KEY IDENTITY(1,1) ,
     IDPedido INT,
     IDProducto INT,
     Cantidad INT NOT NULL,
@@ -72,7 +71,7 @@ CREATE TABLE DetallesPedido (
 
 -- Tabla de reseñas de productos
 CREATE TABLE Reseñas (
-    IDReseña INT PRIMARY KEY,
+    IDReseña INT PRIMARY KEY IDENTITY(1,1),
     IDUsuario INT,
     IDProducto INT,
     Puntuacion INT NOT NULL,
@@ -86,7 +85,7 @@ CREATE TABLE Reseñas (
 
 -- Tabla de carrito de compras
 CREATE TABLE CarritoDeCompras (
-    IDCarrito INT PRIMARY KEY,
+    IDCarrito INT PRIMARY KEY IDENTITY(1,1),
     IDUsuario INT,
     IDProducto INT,
     Cantidad INT,
